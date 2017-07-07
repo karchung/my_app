f58260d6e4c19dbee5f1decdb254cbaadeddec8e
karchung <kelly_archung@yahoo.com>
2017-07-07 16:54:46 -0700

    5.2 removed case-sensitivity from search form

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ApplicationRecord
-	def self.search(search_term) 
-	  Product.where("name LIKE ?", "%#{search_term}%") 
+	def self.search(search_term)
+		if Rails.env.production?
+			Product.where("name ilike ?", "%#{search_term}%")
+		elsif Rails.env.development?
+			Product.where("name LIKE ?", "%#{search_term}%")
+		end
 	end
 end
