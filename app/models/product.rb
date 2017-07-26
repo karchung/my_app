93987dbc800b24c112ccdd96e3b89c4f6b0f27e3
Kelly Archung <kelly_archung@yahoo.com>
2017-07-26 06:28:02 +0000

    5.7 added comment model and styled show page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+	
 	def self.search(search_term)
 		if Rails.env.production?
 			Product.where("name ilike ?", "%#{search_term}%")
@@ -6,4 +7,11 @@ class Product < ApplicationRecord
 			Product.where("name LIKE ?", "%#{search_term}%")
 		end
 	end
+	
+	has_many :comments
+	
+	def highest_rating_comment 
+	  comments.rating_desc.first 
+	end
+	
 end
