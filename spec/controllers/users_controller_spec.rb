2be34211984a64435ade167ce48a4454474428b1
Kelly Archung <kelly_archung@yahoo.com>
2017-08-01 16:49:53 +0000

    6.3 updated test code in user controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,33 +2,33 @@ require 'rails_helper'
  
 describe UsersController, type: :controller do 
   let(:user) { User.create( :email => "kelly@test1.com", :password => "testing")}
-  
-  let(:user) { User.create( :email => "kelly@test2.com", :password => "testing")}
+  let(:user_two) { User.create( :email => "kelly@test2.com", :password => "testing")}
 
   describe 'GET #show' do 
     context 'when a user is logged in' do 
       before do 
         sign_in user
       end
-      
       it 'loads correct user details' do
         get :show, params: { id: user.id }
-        expect(response).to eq(200)
+        expect(response).to have_http_status(200)
         expect(assigns(:user)).to eq user
       end
       
-      it 'redirects to root path' do 
-        get :show, params: { id: user.id } 
-        expect(response).to redirect_to(root_path) 
-      end 
-    end 
-  
     context 'when a user is not logged in' do 
       it 'redirects to login' do 
         get :show, params: { id: user.id } 
         expect(response).to redirect_to(new_user_session_path) 
       end 
     end
-  end
+      
+    context 'one user cannot view another' do 
+      it 'redirects to root path' do 
+        get :show, params: { id: user_two.id } 
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path) 
+      end 
+    end 
   
+  end
 end
