e168f370840cdc6cc635ac52785cfb9f6f41ff2b
Kelly Archung <kelly_archung@yahoo.com>
2017-08-11 07:54:25 +0000

    6.3 updated rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,26 +54,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
 
 require 'spec_helper'
 require 'rspec/rails'
 # note: require 'devise' after require 'rspec/rails'
 require 'devise'
-
-RSpec.configure do |config|
-  # For Devise >= 4.1.0
-  config.include Devise::Test::ControllerHelpers, :type => :controller
-  # Use the following instead if you are on Devise <= 4.1.1
-  # config.include Devise::TestHelpers, :type => :controller
-end
-
-require_relative 'support/controller_macros' # or require_relative '../controller_macros' if write in `spec/support/devise.rb`
-
-RSpec.configure do |config|
-  # For Devise >= 4.1.1
-  config.include Devise::Test::ControllerHelpers, :type => :controller
-  # Use the following instead if you are on Devise <= 4.1.0
-  # config.include Devise::TestHelpers, :type => :controller
-  config.extend ControllerMacros, :type => :controller
-end
