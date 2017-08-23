afd601b6ee1fac719d643765175e06eca5dd9baf
Kelly Archung <kelly_archung@yahoo.com>
2017-08-23 22:34:25 +0000

    5.11 updated users_controller_spec.rb and product_spec.rb with updated email addresses

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,35 +1,42 @@
 require 'rails_helper' 
  
 describe UsersController, type: :controller do 
-  @user = FactoryGirl.create(:user, email: 'kelly@test1.com', password: 'testing')
-  @user2 = FactoryGirl.create(:user, email: 'kelly@test2.com', password: 'testing')
+  let(:user) { FactoryGirl.create(:user, email: "kelly#{rand(100)}@test.com", password: 'testing') }
+  let(:user2) { FactoryGirl.create(:user, email: "kelly#{rand(100)}@test2.com", password: 'testing') }
 
   describe 'GET #show' do 
     context 'when a user is logged in' do 
-      before do 
-        sign_in @user
+      before do
+        sign_in user
       end
       it 'loads correct user details' do
-        get :show, params: { id: @user.id }
+        get :show, params: { id: user.id }
         expect(response).to have_http_status(200)
-        expect(assigns(:user)).to eq @user
+        expect(assigns(:user)).to eq user
       end
     end
-      
+    
+    # this context needs to be ouside the one that signs in the user
     context 'when a user is not logged in' do 
       it 'redirects to login' do 
-        get :show, params: { id: @user.id } 
+        get :show, params: { id: user.id } 
         expect(response).to redirect_to(new_user_session_path) 
       end 
     end
-      
-    context 'one user cannot view another' do 
-      it 'redirects to root path' do 
-        get :show, params: { id: @user2.id } 
+    
+    context 'one user cannot view another' do
+      # here the first user needs to be logged in so that he/she gets
+      # redirected to the root path (if not the redirect goes to the login page)
+      before do
+        sign_in user
+      end
+
+      it 'redirects to root path' do
+        get :show, params: { id: user2.id }
         expect(response).to have_http_status(302)
         expect(response).to redirect_to(root_path) 
       end 
     end 
   
   end
-end
+end 
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 describe Product do
   context "when the product has comments" do
     let(:product) { Product.create!(name: "sweatshirt") }
-    
-    @user = FactoryGirl.create(:user, email: 'kelly@test1.com', password: 'testing')
+    # Here the factory is generating or attempting to generate several users with the 
+    # email 'kelly@test1.com', to prevent this use ruby rand()
+    let(:user) { FactoryGirl.create(:user, email: "kelly-other-#{rand(100)}@test.com", password: 'testing') }
     
     before do 
       product.comments.create!(rating: 1, user: user, body: "Awful sweatshirt!") 
